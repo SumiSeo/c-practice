aeb77be5c0d8b2dd6d9a670c99e9d705228fc603
Sumi <equepem@gmail.com>
2023-05-29 14:46:59 +0200

    number, constants, comments

--- main.c
+++ main.c
@@ -1,23 +1,33 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
 int main()
 {
-    //What type of data ? 1.char 2. int
+    // What type of data ? 1.char 2. int
     int age = 29;
     double gpa = 3.7;
     double temperature = 37.0;
     char grade = 'A';
 
-    //Special type of data type
-    char phrase[] = "Sumi is pretty"; 
+    // Special type of data type
+    char phrase[] = "Sumi is pretty";
 
-
-    printf(phrase);
-    printf("\nHello world\n");
-    printf("I am %d years old.",age);
-    printf("\n");
+   
+    /*printf("\nHello world\n");
+    printf("I am %d years old.", age);
+    printf("\n");*/
     printf("I heard that %s. Yeah, me too. I think it is so true", phrase);
 
+    // working with numbers
+    int num = 6;
+    printf("%f", pow(2,3) );
+
+
+
+    //constants
+    const int NUMBER =5 ;
+    printf("This is number %d",NUMBER);
+    
     return 0;
 }
