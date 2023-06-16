50f122918c50e0f9b7da700bdcd730a2ae95cbc2
Sumi <equepem@gmail.com>
2023-06-16 09:44:34 +0200

    pointer concept

--- main.c
+++ main.c
@@ -1,33 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include <string.h>
+
+
 
 int main()
-{
-    // What type of data ? 1.char 2. int
-    int age = 29;
-    double gpa = 3.7;
-    double temperature = 37.0;
-    char grade = 'A';
+{	
+	int age = 30;
+	double gpa = 3.4;
+	char grade = 'A';
+	int * pAge = &age;
+	double * pGpa = &gpa;
+	char * pGrade = &grade;
 
-    // Special type of data type
-    char phrase[] = "Sumi is pretty";
-
-   
-    /*printf("\nHello world\n");
-    printf("I am %d years old.", age);
-    printf("\n");*/
-    printf("I heard that %s. Yeah, me too. I think it is so true", phrase);
-
-    // working with numbers
-    int num = 6;
-    printf("%f", pow(2,3) );
-
-
-
-    //constants
-    const int NUMBER =5 ;
-    printf("This is number %d",NUMBER);
-    
-    return 0;
+	printf("age's memory address is %p", pAge);
+	printf("age's memory address is %p", pGpa);
+	printf("age's memory address is %p", pGrade);
+	return 0;
 }
+
+
+// return type of function//
+
